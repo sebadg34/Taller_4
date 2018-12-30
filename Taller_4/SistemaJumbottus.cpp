fb774d135f137d64ff92a73d7806b84b88d012d4
sebadg <sebadg98@gmail.com>
2018-12-30 14:11:26 -0300

    Base para taller 4

---
+++ Taller_4/SistemaJumbottus.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include "SistemaJumbottus.h"
+
+
+SistemaJumbottus::SistemaJumbottus()
+{
+	bono = 0;
+	Menu();
+}
+
+
+SistemaJumbottus::~SistemaJumbottus()
+{
+}
+
+void SistemaJumbottus::Menu()
+{
+
+
+
+
+}
+
+int SistemaJumbottus::solucion_en_paralelo()
+{
+	return 0;
+}
+
+int SistemaJumbottus::solucion_secuencial()
+{
+	return 0;
+}
+
+int SistemaJumbottus::Bono_Programador()
+{
+	return 0;
+}
+
+int SistemaJumbottus::leerVentas_secuencial(string producto)
+{
+	return 0;
+}
+
+int SistemaJumbottus::leerVentas_paralelismo(string producto)
+{
+	return 0;
+}
+
+
---
+++ Taller_4/SistemaJumbottus.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cctype>
+#include <algorithm>
+#include <windows.h>
+#include <mmsystem.h>
+#include <math.h>
+
+using namespace std;
+class SistemaJumbottus
+{
+public:
+	int bono;//variable para almacenar el bono
+	SistemaJumbottus();
+	~SistemaJumbottus();
+	void Menu();//menu del sistema
+	int solucion_en_paralelo();//solucion en paralelo
+	int solucion_secuencial();//solucion secuencial
+	int Bono_Programador();//calcula el bono del programador.
+	int leerVentas_secuencial(string producto); //Envia nombre de un producto, lee cada venta y devuelve el valor de las cantidades de ventas de ese producto.
+	int leerVentas_paralelismo(string producto); //Envia nombre de un producto, lee cada venta y devuelve el valor de las cantidades de ventas de ese producto.
+};
+
